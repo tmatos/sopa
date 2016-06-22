aeaeaaa83742b72660a697330872d71b1d631b52
Tiago Matos <tiagocoder@yahoo.com.br>
2016-06-22 04:58:35 -0300

    Agora lendo o json do request guardado no banco, na collection tarefas.

--- requests.py
+++ requests.py
@@ -14,70 +14,40 @@ import config
 
 #connect to database
 connection = MongoClient('localhost', 27017)
-
 db = connection.SOPA1
 
-while 1:    
-    url = "https://www.googleapis.com/qpxExpress/v1/trips/search?key=" + config.key
+url = "https://www.googleapis.com/qpxExpress/v1/trips/search?key=" + config.key
+
+while 1:
+    cursor = db.tarefas.find()
     
-    code = {
-        "request": {
-            "passengers": {
-                "adultCount": 1,
-                "childCount": 0
-            },
-            "slice": [
-                {
-                    "origin": "SSA",
-                    "destination": "GRU",
-                    "date": "2016-07-19",
-                    "permittedDepartureTime":
-                    {
-                        "kind": "qpxexpress#timeOfDayRange",
-                        "earliestTime": "22:00",
-                        "latestTime": "23:00"
-                    }
-                },
-                {
-                    "origin": "GRU",
-                    "destination": "SSA",
-                    "date": "2016-12-25",
-                    "permittedDepartureTime":
-                    {
-                        "kind": "qpxexpress#timeOfDayRange",
-                        "earliestTime": "05:00",
-                        "latestTime": "12:00"
-                    }
-                }
-            ],
-            "solutions": 3
-        }
-    }
+    for it in cursor:
+        code = it['code']
     
-    jsonreq = json.dumps(code, encoding = 'utf-8')
-    
-    headers = {'Content-Type': 'application/json'}
-    req = urllib2.Request(url, jsonreq, headers)
-    
-    flight = urllib2.urlopen(req)
-    
-    response = flight.read()
-    flight.close()
-    
-    obj_resp = json.loads(response)
-    obj_time = datetime.datetime.now()
-    obj_ponto = { "tarefa" : 1, "time" : obj_time, "response" : obj_resp }
-    texto = json.dumps(obj_ponto, default=json_util.default)
-    
-    # para debug
-    #v_file = open("ssaGRU1.json", "a")
-    #v_file.write(texto)
-    #v_file.close()
-    
-    
-    # salva no banco
-    db.pontos.insert_one(obj_ponto)
-    
-    print 'INFO: Obteve ponto da tarefa ', obj_ponto["tarefa"]
+        jsonreq = json.dumps(code, encoding = 'utf-8')
+        
+        headers = {'Content-Type': 'application/json'}
+        req = urllib2.Request(url, jsonreq, headers)
+        
+        flight = urllib2.urlopen(req)
+        
+        response = flight.read()
+        flight.close()
+        
+        obj_resp = json.loads(response)
+        obj_time = datetime.datetime.now()
+        obj_ponto = { "tarefa" : it['numero'], "time" : obj_time, "response" : obj_resp }
+        texto = json.dumps(obj_ponto, default=json_util.default)
+        
+        # para debug
+        #v_file = open("ssaGRU1.json", "a")
+        #v_file.write(texto)
+        #v_file.close()
+        
+        
+        # salva no banco
+        db.pontos.insert_one(obj_ponto)
+        
+        print 'INFO: Obteve ponto da tarefa ', obj_ponto["tarefa"]
     
     time.sleep(20)
